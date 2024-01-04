c57d41319e1c717cfdbbc5caa2f627df5abc8449
marwa alouini <marwaaloui123@gmail.com>
2024-01-04 04:53:06 -0800

    m1

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express();
 
 const port = 5000;
 
-const checkWorkingHours = (req, res, next) => {
-    const currentDate = new Date();
-    const dayOfWeek = currentDate.getDay();
-    const currentHour = currentDate.getHours();
+// const checkWorkingHours = (req, res, next) => {
+//     const dayOfWeek = new Date().getDay();
+//     const currentHour = new Date().getHours();
   
-    // Check if it's a weekday (Monday to Friday) and time is between 9 and 17
-    if (dayOfWeek >= 1 && dayOfWeek <= 5 && currentHour >= 9 && currentHour <= 17) {
-        next(); // Continue to the next middleware/route
-    } else {
-        res.status(403).send('The web application is only available during working hours (Monday to Friday, 9h to 17h).');
-    }
-};
+//     // Check if it's a weekday (Monday to Friday) and time is between 9 and 17
+//     // if (dayOfWeek >= 1 && dayOfWeek <= 5 && currentHour >= 9 && currentHour <= 17) {
+//         next(); // Continue to the next middleware/route
+//     } else {
+//         res.status(403).send('The web application is only available during working hours (Monday to Friday, 9h to 17h).');
+//     }
+// };
+// app.use(checkWorkingHours);
 
 app.use(express.static(__dirname + "/projet"));
 
